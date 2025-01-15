117482654f84f46eaf42b7108c189e66272d517d
Lavliet-PC\Lavliet <mantigor2@gmail.com>
2025-01-15 17:25:09 +0500

    Test CI/CD #13 Final?

--- PowerShell/scripts.ps1
+++ PowerShell/scripts.ps1
@@ -6,5 +6,38 @@ docker build -t heliostavern-backend:latest -f TavernHelios.Server/Dockerfile D:
 docker run -d -p 8080:8080 --name heliostavern-backend heliostavern-backend:latest
 
 
-
 docker down
+
+
+
+
+# Kubernetis
+ssh - p 22 lavliet@ 192.168.0.194
+kubectl delete deployment tavernhelios-server
+kubectl delete service tavernhelios-server-service
+kubectl delete ingress tavernhelios-ingress
+
+cd /home/lavliet/heliostavern-app/
+docker build -t ghcr.io/lavliet2/heliostavern-backend:latest .
+
+
+
+kubectl apply -f /home/lavliet/heliostavern-app/.k8s/tavernhelios-server-deployment.yaml
+kubectl apply -f /home/lavliet/heliostavern-app/.k8s/tavernhelios-ingress.yaml
+
+kubectl get pods
+kubectl describe pod tavernhelios-server-6b6cd58df9-zqlmm
+kubectl get services
+kubectl get ingress
+kubectl logs tavernhelios-server-6b6cd58df9-zqlmm
+kubectl get svc
+
+
+kubectl logs -n ingress-nginx ingress-nginx-controller-tavernhelios-server-6b6cd58df9-zqlmm
+kubectl logs -n ingress-nginx tavernhelios-server-6b6cd58df9-zqlmm
+
+
+kubectl exec -it tavernhelios-server-6b6cd58df9-zqlmm -- /bin/bash
+kubectl cp tavernhelios-server-6b6cd58df9-zqlmm:/var/www/html/config/config.php ./config.php
+
+tavernhelios-server-6b67747d4f-lzsjp
