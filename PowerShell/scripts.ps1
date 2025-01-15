2ea13c71dbe7281dc744ebfc031c2d0e742201eb
Lavliet-PC\Lavliet <mantigor2@gmail.com>
2025-01-15 11:50:28 +0500

    Test CI/CD #11 Change ingress

--- PowerShell/scripts.ps1
+++ PowerShell/scripts.ps1
@@ -1,5 +1,10 @@
 docker build -t heliostavern-frontend .
 docker run -d -p 63049:80 --name TavernHelios.Client heliostavern-frontend
 
+docker build -t heliostavern-backend .
+docker build -t heliostavern-backend:latest -f TavernHelios.Server/Dockerfile D:\Projects\TavernHelios
+docker run -d -p 8080:8080 --name heliostavern-backend heliostavern-backend:latest
+
+
 
 docker down
