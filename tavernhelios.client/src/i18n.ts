c05f56c699bd9dd71be2a655bcf2e70717501013
Lavliet-PC\Lavliet <mantigor2@gmail.com>
2025-01-21 21:40:38 +0500

    CI/CD Настроен автодеплой menu service and mongoDB

--- tavernhelios.client/src/i18n.ts
+++ tavernhelios.client/src/i18n.ts
@@ -10,7 +10,7 @@ i18n.use(initReactI18next).init({
         login: "Login",
         password: "Password",
         signIn: "Sign In",
-        home: "Home TEST",
+        home: "Home",
         menu: "Menu",
         forecast: "Forecast",
         about: "About",
