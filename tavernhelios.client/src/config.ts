db08750c7a319571fe8443522ca351d405f272d8
Lavliet-PC\Lavliet <mantigor2@gmail.com>
2025-02-05 20:23:37 +0500

    Update 21: Connection front to back

---
+++ tavernhelios.client/src/config.ts
@@ -0,0 +1,13 @@
+const getApiBaseUrl = () => {
+    if (import.meta.env.MODE === "development") {
+        return "http://localhost:5040"; 
+    }
+    
+    if (typeof window !== "undefined" && window.VITE_API_URL) {
+        return window.VITE_API_URL;
+    }
+  
+    return "http://178.72.83.217:32040"; // Фоллбек, если ничего не передано
+};
+
+export const API_BASE_URL = getApiBaseUrl();
