5a91e9203a6fb45e9675d723817faa7d0aafbc31
Lavliet-PC\Lavliet <mantigor2@gmail.com>
2025-02-05 11:03:09 +0500

    Update 5: connectionString

--- PowerShell/gRPC.ps1
+++ PowerShell/gRPC.ps1
@@ -1,5 +1,6 @@
+###Local
+#MenuService
 grpcurl -plaintext localhost:5064 list    
-
 grpcurl -plaintext localhost:5064 describe TavernHelios.GrpcContract.MenuService
 grpcurl -plaintext -d '{}' localhost:5064 TavernHelios.GrpcContract.MenuService.GetAllDishes
 
@@ -8,3 +9,8 @@ grpcurl -plaintext localhost:5065 list
 
 grpcurl -plaintext localhost:5065 describe TavernHelios.GrpcContract.ReservationService
 grpcurl -plaintext -d '{}' localhost:5065 TavernHelios.GrpcContract.ReservationService
+
+###Kubernetes
+#ReservationService
+grpcurl -plaintext 178.72.83.217:32042 list   
+grpcurl -plaintext 178.72.83.217:32042 describe TavernHelios.GrpcContract.ReservationService
