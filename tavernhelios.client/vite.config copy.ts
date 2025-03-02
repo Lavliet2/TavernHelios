ff13e83d73d2a10941ec218d924b78fb6823be27
Lavliet-PC\Lavliet <mantigor2@gmail.com>
2025-03-02 18:06:16 +0500

    Change vite

--- tavernhelios.client/vite.config copy.ts
+++ tavernhelios.client/vite.config copy.ts
@@ -24,7 +24,7 @@ console.log("NODE_ENV from process:", env.NODE_ENV);
 console.log("VITE_API_URL from process:", env.VITE_API_URL);
 
 // const target = env.VITE_API_URL || `http://178.72.83.217:32040`;
-const target = env.VITE_API_URL || `https://tavernhelios.duckdns.org/api`;
+const target = env.VITE_API_URL || `https://tavernhelios.duckdns.org`;
 // const target = `https://tavernhelios.duckdns.org`;
 // const target = env.VITE_API_URL 
 //     || (!isDocker ? `https://localhost:5555` : `http://178.72.83.217:32040`);
@@ -49,7 +49,7 @@ export default defineConfig({
         target,
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, '/api')
       }
     },
     port: 8888,
