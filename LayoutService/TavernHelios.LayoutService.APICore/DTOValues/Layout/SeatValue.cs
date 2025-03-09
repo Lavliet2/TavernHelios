6f2be49a750a1f54cb1cdf6fc5857f9feeeafc35
Mark Britenkov <britenkov.ms@gmail.com>
2025-03-09 19:12:31 +0300

    Сервис схемы зала

---
+++ LayoutService/TavernHelios.LayoutService.APICore/DTOValues/Layout/SeatValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TavernHelios.LayoutService.APICore.DTOValues.Layout
+{
+    public class SeatValue
+    {
+        public int Number { get; set; } = 0;
+        public string Desctiption { get; set; } = string.Empty;
+
+        public PointValue Center { get; set; } = new PointValue();
+        public PointValue Alignment { get; set; } = new PointValue();
+    }
+}
---
+++ LayoutService/TavernHelios.LayoutService.MongoRepositories/Repositories/LayoutRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Options;
+using TavernHelios.LayoutService.APICore.Entities.Layout;
+using TavernHelios.LayoutService.Common.Settings;
+
+namespace TavernHelios.LayoutService.MongoRepositories.Repositories
+{
+    public class LayoutRepository : BaseMongoRepository<LayoutEntity>
+    {
+        public LayoutRepository(
+            IOptions<LayoutMongoConnectionSettings> mongoSettings
+            ) : base(mongoSettings)
+        {
+            
+        }
+    }
+}
