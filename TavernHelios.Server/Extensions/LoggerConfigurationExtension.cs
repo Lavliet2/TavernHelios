ee2ff537d68690b29bc2270cee761a0954eab3d7
Lavliet-PC\Lavliet <mantigor2@gmail.com>
2025-04-30 20:25:04 +0500

    Clean commit

--- TavernHelios.Server/Extensions/LoggerConfigurationExtension.cs
+++ TavernHelios.Server/Extensions/LoggerConfigurationExtension.cs
@@ -44,39 +44,5 @@ namespace TavernHelios.Server.Extensions
                     })
                 );
         }
-
-        //public static LoggerConfiguration Configure(
-        //    this LoggerConfiguration logger,
-        //     IConfiguration configuration)
-        //{
-        //    var assemblyName = Assembly.GetEntryAssembly()?.GetName().Name ?? "Unknown";
-        //    return logger
-        //        .Enrich.WithProperty("Application", assemblyName) // Добавляем поле Application
-        //        .Enrich.FromLogContext() // Для добавления дополнительных свойств из контекста
-        //        //.WriteTo.Console(new JsonFormatter(renderMessage: true))
-        //        .WriteTo.Logger(lc => lc
-        //            .Filter.ByIncludingOnly(logEvent =>
-        //                logEvent.Properties.TryGetValue("SourceContext", out var value) &&
-        //                value.ToString() == "\"Middleware\"") // Фильтруем логи по контексту
-        //            .WriteTo.RabbitMQ((clientConfiguration, sinkConfiguration) =>
-        //            {
-        //                clientConfiguration.Hostnames.Add(configuration["RABBITMQ_HOST"] ?? "localhost");
-        //                clientConfiguration.Username = configuration["RABBITMQ_USER"] ?? "admin";
-        //                clientConfiguration.Password = configuration["RABBITMQ_PASSWORD"] ?? "admin";
-        //                clientConfiguration.Exchange = "logs_exchange";
-        //                clientConfiguration.ExchangeType = "direct";
-        //                clientConfiguration.DeliveryMode = RabbitMQDeliveryMode.Durable;
-        //                clientConfiguration.RoutingKey = "log";
-        //                clientConfiguration.Port =
-        //                   Int32.TryParse(configuration["RABBITMQ_PORT"], out var res) ?
-        //                   res :
-        //                   5672;
-        //                clientConfiguration.VHost = "/";
-        //                clientConfiguration.Heartbeat = 60;
-
-        //                sinkConfiguration.TextFormatter = new LogJsonFormatter();
-        //            })
-        //        );
-        //}
     }
 }
