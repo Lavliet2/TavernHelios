62dcac9e520218426d8ef992da18e3b7241efb4d
Lavliet-PC\Lavliet <mantigor2@gmail.com>
2025-05-23 20:22:37 +0500

    Deploy to prod

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -22,12 +22,14 @@ kubectl apply -f .k8s/clickhouse-monitoring-deployment.yaml
 kubectl apply -f .k8s/rabbitmq-monitoring-deployment.yaml
 kubectl apply -f .k8s/prometheus-monitoring-deployment.yaml
 kubectl apply -f .k8s/grafana-monitoring-deployment.yaml
+kubectl apply -f .k8s/redis-weather-deployment.yaml
 
 kubectl apply -f .k8s/menuService-deployment.yaml
 kubectl apply -f .k8s/reservationService-deployment.yaml
 kubectl apply -f .k8s/layoutService-deployment.yaml
 kubectl apply -f .k8s/authService-deployment.yaml
 kubectl apply -f .k8s/adminService-deployment.yaml
+kubectl apply -f .k8s/weatherService-deployment.yaml
 kubectl apply -f .k8s/tavernhelios-server-deployment.yaml
 kubectl apply -f .k8s/tavernhelios-client-deployment.yaml
 
@@ -37,6 +39,7 @@ kubectl rollout restart deployment reservationservice
 kubectl rollout restart deployment layoutservice
 kubectl rollout restart deployment authservice
 kubectl rollout restart deployment adminservice
+kubectl rollout restart deployment weatherservice
 kubectl rollout restart deployment tavernhelios-server
 kubectl rollout restart deployment tavernhelios-client
 
